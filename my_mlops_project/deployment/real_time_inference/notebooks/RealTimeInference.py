e759385ab84ec298d54cc701d315a8984c33c820
aliazzzdat <115075488+aliazzzdat@users.noreply.github.com>
2024-02-02 19:08:10 +0100

    add import mlflow

--- my_mlops_project/deployment/real_time_inference/notebooks/RealTimeInference.py
+++ my_mlops_project/deployment/real_time_inference/notebooks/RealTimeInference.py
@@ -23,6 +23,7 @@ import os
 import json
 import requests
 import time
+import mlflow
 
 notebook_path =  '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get())
 %cd $notebook_path
